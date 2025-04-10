0a32540f3e16d3071aaaa52a9b39344e93c8f623
priscibull <nicolagfaldi@icloud.com>
2025-04-10 16:26:54 +0200

    Merge branch 'main' of https://github.com/NaoNexus/ChallengeNaoNexus25

--- coding/server/py3/database.py
+++ coding/server/py3/database.py
@@ -1,6 +1,5 @@
 import firebase_admin
 from firebase_admin import credentials, firestore
-from datetime import datetime
 
 # Carica le credenziali
 cred = credentials.Certificate("nao-basket-e5f9e-firebase-adminsdk-fbsvc-7feac96803.json")
@@ -12,11 +11,35 @@ db = firestore.client()
 # Aggiunge infortuni
 def add_injury():
 
-    injury_name = str(input("Nome infortunio: "))
-    recovery = str(input("Metodo di recupero: "))
-    recovery_time = int(input("Tempo di recupero(giorni): "))
+    while True:
+        injury_name = str(input("Injury name: ")).strip().lower()
+        if injury_name:
+            break
+        else:
+            print("Try again.")
 
+
+    while True:
+        recovery = str(input("Recovery method: ")).strip().lower()
+        if recovery:
+            break
+        else:
+            print("Try again.")
+    
+    while True:
+        recovery_time = int(input("Recovery time: "))
+        if recovery_time:
+            break
+        else:
+            print("Try again.")
+    
     doc_ref = db.collection('injuries').document(injury_name)
+    doc = doc_ref.get()
+
+    if doc.exists:
+        print("Injury already exists")
+
+    
     doc_ref.set({
         'Recovery': recovery,
         'Time': recovery_time
@@ -24,117 +47,67 @@ def add_injury():
 
 # Creazione utenti
 def add_players():
-    player_name = str(input("Player's name: "))
-    injury_list = ""
-    now = datetime.now()
-    last_date = now.strftime("%d/%m/%Y")
+
+    while True:
+        player_name = str(input("Player's name: ")).strip().lower()
+        if player_name:
+            break
+        else:
+            print("Try again.")
+
 
     doc_ref = db.collection('players').document(player_name)
-    doc_ref.set({
-        'Injury list': [],
-        'Time': 0,
-        'Last_date': last_date 
-    })
+    doc = doc_ref.get()
 
-# Modifica dati dei giocatori
-def modify_players():
-    player_name = str(input("Player's name: "))
-    injury = str(input("Add_injury: "))
-
-    # Riferimento al documento del giocatore
-    player_ref = db.collection('players').document(player_name)
-    player_doc = player_ref.get()
-
-    # Calcolo del tempo rimanente
-    time = player_doc.get('Time')
-    last_date_str = player_doc.get('Last_date')  # Cambia qui
-    now = datetime.now()
-    current_date_str = now.strftime("%d/%m/%Y")
-
-    last_date = datetime.strptime(last_date_str, "%d/%m/%Y")
-    current_date = datetime.strptime(current_date_str, "%d/%m/%Y")
-
-    # Calcola la differenza in giorni
-    delta = current_date - last_date
-    days = delta.days
-    time -= days
-
-    if time < 0:
-        player_ref.update({
-        'Injury list': []
-        })
-        time = 0 
-
-    # Riferimento al documento dell'infortunio
-    injury_ref = db.collection('injuries').document(injury)
-    injury_doc = injury_ref.get()
-
-    # Controlla se sia il giocatore che l'infortunio esistono
-    if player_doc.exists and injury_doc.exists:
-        # Aggiungi i giorni dell'infortunio
-        time += injury_doc.get("Time")
-        # Aggiungi il nuovo infortuno alla lista esistente del giocatore
-        player_ref.update({
-            'Injury list': firestore.ArrayUnion([injury]),  # Aggiunge l'infortunio all'array
-            'Time': time,  # Aggiorna il campo "Time" (se necessario)
-            'Last_date': current_date_str  # Cambia qui
-        })
-        print(f"Injury '{injury}' added to {player_name}'s injury list.")
+    if doc.exists:
+        print("Player already exists")
     else:
-        if not player_doc.exists:
-            print(f"Player '{player_name}' does not exist.")
-        if not injury_doc.exists:
-            print(f"Injury '{injury}' does not exist.")
+        doc_ref.set({
+            'Injury list': "",
+            'Time': 0,
+            'Last date': 0
+        })
+        print("Player added successfully")
 
-def get_player_info():
-    # Chiedi il nome del giocatore
+# Modifica utenti
+def add_injury_tap():
+    
     player_name = str(input("Player's name: "))
 
-    # Riferimento al documento del giocatore
-    player_ref = db.collection('players').document(player_name)
-    player_doc = player_ref.get()
+    doc_ref = db.collection('players').document(player_name)
+    doc = doc_ref.get()
 
-    # Verifica se il giocatore esiste
-    if not player_doc.exists:
-        print(f"Player '{player_name}' does not exist.")
-        return
+    if doc.exists:
+        
+        injury = str(input("Add_injury: "))
+        doc_ref_i = db.collection('injuries').document(injury)
+        doc_i = doc_ref_i.get()
 
-    # Calcolo del tempo rimanente
-    time = player_doc.get('Time')
-    last_date_str = player_doc.get('Last_date')  # Cambia qui
-    now = datetime.now()
-    current_date_str = now.strftime("%d/%m/%Y")
+        if doc_i.exists:
+            
+            injury_time = doc_ref_i.get("Time") #--!! Errore qui !!
+            time = doc_ref.get("Time") #--!! Errore qui !!
 
-    last_date = datetime.strptime(last_date_str, "%d/%m/%Y")
-    current_date = datetime.strptime(current_date_str, "%d/%m/%Y")
+            if injury_time >= time:
+                time = injury_time
 
-    # Calcola la differenza in giorni
-    delta = current_date - last_date
-    days = delta.days
-    time -= days
+            player_data = doc.to_dict()
+            injury_list = player_data.get('Injury list', [])
+            if not isinstance(injury_list,list):   #controlla se injury_list è di tipo lista
+                injury_list=[injury_list]
+            injury_list.append(injury)
 
-    if time < 0:
-        player_ref.update({
-        'Injury list': []
-        })
-        time = 0
-    
-    # Modifica Last_date e time
-    player_ref.update({
-            'Time': time,
-            'Last_date': current_date_str
-        })
-
-    # Aggiorna la snapshot del documento dopo l'aggiornamento
-    player_doc = player_ref.get()
-
-    # Ottieni tutti i campi del documento
-    player_data = player_doc.to_dict()
-
-    # Stampa i dettagli del giocatore
-    print(f"\nDetails for player '{player_name}':")
-    for field, value in player_data.items():
-        print(f"{field}: {value}")
+            doc_ref.update({
+                'Injury list': injury_list, 
+                'Time': time
+            })
+        
+        else:
+            print("Injury does not exist.")
+            add_injury_tap()
+    else:
+        print("Player does not exist.")
+        add_injury_tap()
 
 # Esempio di lettura di un documento
 def get_document():
@@ -155,7 +128,4 @@ def get_all_documents():
         print(f'{doc.id} => {doc.to_dict()}')
 
 # Esegui le funzioni
-#add_players()
-#modify_players()
-#get_player_info()
-
+add_injury_tap()
